038bc47cbc00ef02fec9e890392b2bce584973b7
Steph974-Git <10466@holbertonstudents.com>
2025-04-08 13:32:23 +0200

    fix: return 0 for height of NULL tree in binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -27,14 +27,14 @@ int max(int a, int b)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
-return (max(binary_tree_height(tree->left),
-binary_tree_height(tree->right)) + 1);
 
+	return (max(binary_tree_height(tree->left),
+	binary_tree_height(tree->right)) + 1);
 
 }
